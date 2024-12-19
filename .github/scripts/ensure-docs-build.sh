03490266e581baabbf87d404bccb2f0c299f94e6
Truman Shuck <tshuck@beyondfinance.com>
2024-12-19 14:27:34 -0500

    Add ci check to enforce updates to static docs file

---
+++ .github/scripts/ensure-docs-build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+if git diff --quiet HEAD origin/main -- docs/src; then
+  echo ''
+  echo '######################'
+  echo '# docs/src unchanged #'
+  echo '######################'
+  echo ''
+else
+  echo ''
+  echo '####################'
+  echo '# docs/src changed #'
+  echo '####################'
+
+  echo '  checking for change to index.html'
+  echo ''
+
+  if git diff --quiet HEAD origin/main -- lib/feature_map/private/docs/index.html; then
+    echo '  ###########'
+    echo '  # failure #'
+    echo '  ###########'
+
+    echo '    please run `npm run build` within the `docs/`` directory'
+    echo '    and commit lib/feature_map/private/docs/index.html'
+    exit 1
+  else
+    echo '###########'
+    echo '# success #'
+    echo '###########'
+
+    echo '    thank you!'
+    exit 0
+  fi
+fi
