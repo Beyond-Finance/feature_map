46e9fc72006c8a5085edd9314447ea22a7d504a3
Truman Shuck <tshuck@beyondfinance.com>
2025-01-03 16:06:20 -0500

    Allow vite site to consume yaml config for use during development

--- docs/src/utils/config.js
+++ docs/src/utils/config.js
@@ -1,5 +1,6 @@
 import sampleConfig from '../data/sample_config';
+import sampleProjectConfig from '../../../.feature_map/config.yml'
 
 export const getConfig = () => {
-  return window.FEATURE_MAP_CONFIG || sampleConfig
+  return window.FEATURE_MAP_CONFIG || { ...sampleConfig, project: sampleProjectConfig, environment: { commit_sha: 'main' } }
 }
