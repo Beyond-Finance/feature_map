affc16c48452c057f924ff3016b8f781d895dedf
Harrison Strowd <hstrowd@beyondfinance.com>
2025-01-11 07:04:55 -0800

    DO NOT MERGE!!! Replacing the docs folder with the Github Pages site content just to test the configuration and resulting site content served by Github Pages.

--- docs/src/utils/config.js
+++
@@ -1,5 +0,0 @@
-import sampleConfig from '../data/sample_config';
-import sampleProjectConfig from '../../../.feature_map/config.yml'
-
-const defaultConfig = { ...sampleConfig, project: sampleProjectConfig, environment: { git_ref: 'main' } }
-export const config = window.FEATURE_MAP_CONFIG || defaultConfig
