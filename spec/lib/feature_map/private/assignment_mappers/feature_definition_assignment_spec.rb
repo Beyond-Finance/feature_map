ed680585857d065ab3ae647702ae4414bce001c0
Truman Shuck <tshuck@beyondfinance.com>
2025-03-10 13:13:43 -0400

    Back out testing change

--- spec/lib/feature_map/private/assignment_mappers/feature_definition_assignment_spec.rb
+++ spec/lib/feature_map/private/assignment_mappers/feature_definition_assignment_spec.rb
@@ -1,14 +1,10 @@
 module FeatureMap
   RSpec.describe Private::AssignmentMappers::FeatureDefinitionAssignment do
     before do
-      write_configuration('assigned_globs' => '**/*')
-      write_file('.feature_map/feature_definitions.csv', <<~CSV.strip)
-        Name,Description,Documentation Link,Dashboard Link,Custom Attribute
-        Bar,,,,
-      CSV
-      write_file('foo.rb', <<~FILE.strip)
-        # @feature Bar
-      FILE
+      write_configuration
+      write_file('.feature_map/definitions/bar.yml', <<~CONTENTS)
+        name: Bar
+      CONTENTS
     end
 
     describe 'FeatureMap.for_feature' do
