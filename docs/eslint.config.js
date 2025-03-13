1c9e70eb576e041c06506050c559353cc5d06b11
Truman Shuck <tshuck@beyondfinance.com>
2025-03-13 16:23:15 -0400

    Install prettier, eslint

---
+++ docs/eslint.config.js
@@ -0,0 +1,44 @@
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        document: 'readonly',
+        navigator: 'readonly',
+        window: 'readonly',
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react/prop-types': 0, // Not implemented in this project
+      'react/react-in-jsx-scope': 0, // Not required React >= 17
+    },
+  },
+  prettierConfig,
+  {
+    ignores: ['node_modules/**', 'dist/**', 'build/**'],
+  },
+];
